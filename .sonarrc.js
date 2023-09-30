6dc9321a879de0eb8425861462d51894f628c902
thurairaj <thurairaj.l@setel.my>
2023-09-30 18:22:27 +0800

    Test nestjs

---
+++ .sonarrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'sonarjs'],
+  extends: [
+    "plugin:sonarjs/recommended",
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'sonarjs/no-duplicate-string': 'off',
+  },
+};
